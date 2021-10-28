3b2a2f4d6069194c2798585e5518feeb51573331
Fabrice ROSAMOND <fabrice.rosamond@hotmail.fr>
2021-10-28 19:00:01 +0200

    modification du script permettat=nt de relier html et javascript

--- calculatrice.js
+++ calculatrice.js
@@ -2,15 +2,22 @@ var chaineOperation = new String();
 
 function concatenerOperation(touche) {
     chaineOperation = chaineOperation.concat(touche);
+    updateAffichage();
     console.log("concatenerOperations -> chaineOperation: " + chaineOperation);
 }
 
 function calculResultat() {
     chaineOperation = eval(chaineOperation);
+    updateAffichage();
     console.log("calculResultat -> chaineOperation: " + chaineOperation);
 }
 
 function effacerResultat() {
     chaineOperation = "";
+    updateAffichage();
     console.log("effacerResultat -> chaineOperation: " + chaineOperation);
+}
+
+function updateAffichage() {
+    //document.getElementById("mytext").value = "My value";
 }
